a2453f53d3b079671a7bda627237f3dff4d78b96
Armasangel <angel.armasher@gmail.com>
2026-08-26 22:13:24 -0600

    Cambio en los iconos del proyecto, de png a svg.

--- js/data/Icons.js
+++ js/data/Icons.js
@@ -1,24 +1,33 @@
 // Rutas de iconos de la wiki de Genshin Impact (Fandom).
 
 export const PIECE_ICONS = {
-    FLOWER:  'js/images/Icon_Flower_of_Life.png',
-    PLUME:   'js/images/Icon_Plume_of_Death.png',
-    SANDS:   'js/images/Icon_Sands_of_Eon.png',
-    GOBLET:  'js/images/Icon_Goblet_of_Eonothem.png',
-    CIRCLET: 'js/images/Icon_Circlet_of_Logos.png',
+    FLOWER:  'js/images/Icon_Flower_of_Life.svg',
+    PLUME:   'js/images/Icon_Plume_of_Death.svg',
+    SANDS:   'js/images/Icon_Sands_of_Eon.svg',
+    GOBLET:  'js/images/Icon_Goblet_of_Eonothem.svg',
+    CIRCLET: 'js/images/Icon_Circlet_of_Logos.svg',
 };
 
 // Un icono de atributo puede representar varias StatType (ej. ATK_FLAT y ATK_PERCENT
 // comparten el mismo icono de "Ataque" en la wiki).
 export const STAT_ICONS = {
-    CRIT_RATE:          'js/images/Icon_Attribute_Critical_RATE.webp',
-    CRIT_DMG:           'js/images/Icon_Attribute_Critical_HIT.png',
-    ATK_PERCENT:        'js/images/Icon_Attribute_Attack.png',
-    ATK_FLAT:           'js/images/Icon_Attribute_Attack.png',
-    HP_PERCENT:         'js/images/Icon_Attribute_Hp.png',
-    HP_FLAT:            'js/images/Icon_Attribute_Hp.png',
-    DEF_PERCENT:        'js/images/Icon_Attribute_Defense.png',
-    DEF_FLAT:           'js/images/Icon_Attribute_Defense.png',
-    ENERGY_RECHARGE:    'js/images/Icon_Attribute_Energy_Recharge.png',
-    ELEMENTAL_MASTERY:  'js/images/Icon_Attribute_Elemental_Mastery.png',
+    CRIT_RATE:          'js/images/Icon_Attribute_Critical_RATE.svg',
+    CRIT_DMG:           'js/images/Icon_Attribute_Critical_HIT.svg',
+    ATK_PERCENT:        'js/images/Icon_Attribute_Attack.svg',
+    ATK_FLAT:           'js/images/Icon_Attribute_Attack.svg',
+    HP_PERCENT:         'js/images/Icon_Attribute_Hp.svg',
+    HP_FLAT:            'js/images/Icon_Attribute_Hp.svg',
+    DEF_PERCENT:        'js/images/Icon_Attribute_Defense.svg',
+    DEF_FLAT:           'js/images/Icon_Attribute_Defense.svg',
+    ENERGY_RECHARGE:    'js/images/Icon_Attribute_Energy_Recharge.svg',
+    ELEMENTAL_MASTERY:  'js/images/Icon_Attribute_Elemental_Mastery.svg',
+    HEALING_BONUS:       'js/images/Icon_Attribute_Healing_Bonus.svg',
+    PYRO_DMG_BONUS:     'js/images/Icon_Attribute_Pyro_Damage_Bonus.svg',
+    HYDRO_DMG_BONUS:     'js/images/Icon_Attribute_Hydro_Damage_Bonus.svg',
+    CRYO_DMG_BONUS:      'js/images/Icon_Attribute_Cryo_Damage_Bonus.svg',
+    ELECTRO_DMG_BONUS:   'js/images/Icon_Attribute_Electro_Damage_Bonus.svg',
+    ANEMO_DMG_BONUS:     'js/images/Icon_Attribute_Anemo_Damage_Bonus.svg',
+    GEO_DMG_BONUS:       'js/images/Icon_Attribute_Geo_Damage_Bonus.svg',
+    DENDRO_DMG_BONUS:    'js/images/Icon_Attribute_Dendro_Damage_Bonus.svg',
+    PHYSICAL_DMG_BONUS:  'js/images/Icon_Attribute_Physical_Damage_Bonus.svg',
 };
